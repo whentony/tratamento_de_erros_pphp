31f6d9a07707db6db249bcad78343c2f31505861
whentony <whentony.ferreira@gmail.com>
2021-04-21 23:34:03 -0300

    Modulo I e II do Curso

---
+++ pilha.php
@@ -0,0 +1,40 @@
+<?php
+//pilha
+
+//debug_backtrace()  ->  Mostra a trilha da pilha
+
+/*
+Fatal error: Quando não há tratamento de erro e o php matou o processo
+*/
+
+function funcao1(){
+    echo "Entrei na função 1". PHP_EOL;
+    try{
+        funcao2();
+    } catch(DivisionByZeroError | RuntimeException $problema){
+        echo $problema->getMessage(). PHP_EOL;
+        echo $problema->getLine(). PHP_EOL;
+        echo $problema->getTraceAsString(). PHP_EOL;
+    } 
+    echo "Saindo da função 1". PHP_EOL;
+}
+
+function funcao2(){
+
+    echo "Entrei na função 2". PHP_EOL;
+    for($i = 0; $i <= 5; $i++){
+        echo $i. PHP_EOL;
+    }
+    $divisao = intdiv(5, 0);
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = "valor"; 
+    
+  
+     
+    echo "Saindo da função 2".PHP_EOL;
+}
+
+echo 'Iniciando programa principal'.PHP_EOL;
+funcao1();
+echo 'Finalizando programa principal'.PHP_EOL;
+?>
